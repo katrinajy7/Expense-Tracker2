d5b7493c19f64fd5dd3ffc9cd8915a96602fe8df
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-09-21 16:15:10 +0800

    first update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      React homework template dissapointed with this course. Not good in students communication!
     </div>
   );
 };
