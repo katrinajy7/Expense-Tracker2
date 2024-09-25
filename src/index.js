c4847ff9801408227e4821af927842dcb1557f0b
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-09-25 17:02:10 +0800

    trial 3

--- src/index.js
+++ src/index.js
@@ -12,15 +12,16 @@
 import  'antd/dist/reset.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { persistor, store } from "../src/redux/store";
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import App from './components/App';
-import { GlobalStyles } from 'styles/GlobalStyles';
+import App from "../src/components/App";
+import { GlobalStyles } from "../src/styles/GlobalStyles";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
